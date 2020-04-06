e072eef56508984ae11b92240870d3a6583bb360
Anatoli Belchikov <belchikov1996@gmail.com>
2020-04-06 11:59:58 +0300

    Initial commit

---
+++ tensorflow-gpu.sh
@@ -0,0 +1,1 @@
+docker run -v $(realpath .):/tf/epilepsy --network host -it --rm --runtime=nvidia tensorflow/tensorflow:1.13.0rc0-gpu-py3-jupyter python -m jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root
